76da72d0509401936666e36c38a971fe6e619bdb
joetime <joetime@gmail.com>
2014-01-11 13:27:43 -0500

    first unit test success

---
+++ cfglib/Repos.cs
@@ -0,0 +1,35 @@
+﻿using cfgdata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cfglib
+{
+    public class HorizonFile {
+
+        public HorizonFile(hzImport import) {
+            Id = import.Id;
+        }
+
+        public int Id { get; private set; }
+        
+    }
+
+    public class Repos
+    {
+        town6668Entities DB = new town6668Entities();
+
+        public HorizonFile HorizonFile (int id) {
+
+            hzImport h = (from import in DB.hzImports
+                    where import.Id == id
+                    select import).First();
+
+            return new HorizonFile(h);
+                       
+        }
+        
+    }
+}
---
+++ cfgtest/HorizonFileTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cfglib;
+
+namespace cfgtest
+{
+    [TestClass]
+    public class HorizonFileTests
+    {
+        [TestMethod]
+        public void GetHorizonFile()
+        {
+            Repos repos = new Repos();
+            HorizonFile file = repos.HorizonFile(18);
+            Assert.IsTrue(file.Id == 18, "Horizon record not found.");
+        }
+
+    }
+}
